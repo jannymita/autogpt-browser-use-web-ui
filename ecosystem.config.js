e35c98a2ecfd09ca5ec0dcb005e2fb529fe91469
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2025-02-02 18:22:37 +0700

    fix: starting script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "autogpt-browser-use-web-ui", // Name of the application
-      script: "source .venv/bin/activate && python webui.py", // Your Python script
+      script: "webui.py", // Your Python script
+      interpreter: ".venv/bin/python", // Specify Python interpreter
       cwd: "./", // Current working directory
       watch: false, // Disable watching for changes (set to true if you want to watch and auto-restart on file changes)
       env: {
