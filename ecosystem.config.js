96f6be7f961c361da33d2b145e3bcb3a3b6fcad2
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2025-02-02 18:22:43 +0700

    fix: starting script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,6 @@ module.exports = {
       interpreter: ".venv/bin/python", // Specify Python interpreter
       cwd: "./", // Current working directory
       watch: false, // Disable watching for changes (set to true if you want to watch and auto-restart on file changes)
-      env: {
-        NODE_ENV: "production", // Set environment variable (optional)
-      },
       log_file: "./logs/webserver.log", // Optional log file path
     },
   ],
